9dd0b2db356fe1b76fee903d4c891db285e776d2
ET-CS <et.programming@gmail.com>
2014-07-20 13:53:47 +0000

    Initial commit

---
+++ checker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Website status checker.
+# save all websites to check in file named 'websites.txt'. each in new line.
+# leave an empty line in the end.
+
+# Quiet mode. false for test purposes. true for cron.
+QUIET=true
+# Send mail in case of failure to:
+EMAIL=erithq@gmail.com
+# list of websites. each website in new line. leave an empty line in the end.
+LISTFILE=/scripts/isOnline/websites.txt
+
+function test {
+  response=$(curl --write-out %{http_code} --silent --output /dev/null $1)
+  #if [ $response -eq 200 ] || [ $response -eq 301 ] ; then
+  if [ $response -eq 200 ] ; then
+      #echo -e "\e[31mDOWN\e[0m"
+  #fi
+  #curl -s -o "/dev/null" $1
+
+  #if wget $1 --timeout=900 --spider -q; then
+    if [ "$QUIET" = false ] ; then
+      echo -n "$response "
+      echo -e "\e[32m[ok]\e[0m"
+    fi
+    if [ -f cache/$1 ]; then
+      rm -f cache/$1
+    fi
+  else
+    if [ "$QUIET" = false ] ; then
+      echo -e "\e[31mDOWN\e[0m"
+    fi
+    if [ ! -f cache/$1 ]; then
+        # using mail command
+        #mail -s "$p WEBSITE DOWN" "$EMAIL"
+        # using mailx command
+        echo "$p WEBSITE DOWN" | mailx -s "$1 WEBSITE DOWN" $EMAIL
+        echo > cache/$p
+    fi
+  fi
+}
+
+while read p; do
+  if [ "$QUIET" = false ] ; then
+    echo -n "$p "
+  fi
+  test $p
+done < $LISTFILE
